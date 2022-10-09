eeda2a80923218df0c28a9581b9101d92b4af52b
Vladislav Terentyev <v.terentyev@themindstudios.com>
2022-10-09 10:24:07 +0300

    verification

--- app.py
+++ app.py
@@ -13,5 +13,14 @@ def me():
     return "astler!"
 
 
+@app.route('/.well-known/pki-validation/948281C097CA441852C3AB70F8720614.txt')
+def verification():
+    path = '/root/var/www/html/.well-known/pki-validation/948281C097CA441852C3AB70F8720614.txt'
+    read = open(path, 'r')
+    data = read.read()
+    read.close()
+    return data
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=80)

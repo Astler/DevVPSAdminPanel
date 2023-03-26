c8a2d1a39c612756b13e71ecde31b98c334f3672
Vladislav Terentyev <v.terentyev@themindstudios.com>
2023-03-26 13:10:51 +0300

    Removed delete logic

--- banners/banners_commands.py
+++ banners/banners_commands.py
@@ -131,9 +131,6 @@ def get_paged_previous_banners():
     dt_obj = datetime.strptime(today, '%Y-%m-%d %H:%M:%S')
     today_milli_seconds = dt_obj.timestamp() * 1000
 
-    db.session.query(DailyBannerItem).delete()
-    db.session.commit()
-
     banners = db.session.query(DailyBannerItem).filter(DailyBannerItem.date < today_milli_seconds).order_by(
         DailyBannerItem.date.desc())
 

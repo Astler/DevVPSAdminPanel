5dc545c29d3f82a164ab737994e1e46dcca46340
Astler <vladyclaw@gmail.com>
2024-12-22 02:50:05 +0200

    Deep valid

--- application/index.py
+++ application/index.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, current_app, redirect, url_for
+from flask import Blueprint, render_template, current_app, redirect, url_for, send_file
 from flask_login import current_user
 
 main_blueprint = Blueprint('main', __name__)
@@ -10,3 +10,7 @@ def index():
         return redirect(url_for('profile_blueprint.profile'))
     return render_template('index.html', sign_up_enabled=current_app.config['SIGN_UP_ENABLED'])
 
+@main_blueprint.route('/.well-known/assetlinks.json')
+def asset_links():
+    return send_file('assetlinks.json',
+                    mimetype='application/json')
